566bdbf6c7fb1ff3098903857035cee060fa4326
charpty <charpty@gmail.com>
2019-03-03 23:25:49 +0800

    修改：关闭代码优化以方便调试

--- run.sh
+++ run.sh
@@ -67,7 +67,7 @@ if test -f autogen.sh ; then
 fi
 echo "Configuring $module_name ..."
 echo "Configuring $module_name ..." >>$LOG_FILE
-sh configure CFLAGS="-g -O2 -fPIC" CXXFLAGS="-g -O2 -fPIC -std=c++0x" $WITHS --prefix=$DST_PATH/ 1>>$LOG_FILE 2>>$ERROR_LOG_FILE
+sh configure CFLAGS="-g -O0 -fPIC" CXXFLAGS="-g -O0 -fPIC -std=c++0x" $WITHS --prefix=$DST_PATH/ 1>>$LOG_FILE 2>>$ERROR_LOG_FILE
 if [ $? -ne 0 ]; then 
     echo "Call configure failed. MODULE:$module_name"
     echo "Call configure failed. MODULE:$module_name" >>$ERROR_LOG_FILE
